8bf6c062a4fcbd68ae2959be08311a27efd774b4
Mansoor S <mansoor@msiddeeq.com>
2021-01-29 02:00:11 -0500

    setup prisma client

---
+++ server/src/generated/prisma-client/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Link",
+    embedded: false
+  },
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Vote",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/mansoor-siddeeq-43a7b2/hackernews-react-urql/dev`
+});
+exports.prisma = new exports.Prisma();
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
+import { cacheExchange } from '@urql/exchange-graphcache';
+
+const cache = cacheExchange({});
+
+const client = new Client({
+  url: 'http://localhost:3001/',
+  exchanges: [ dedupExchange, cache, fetchExchange ],
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider value={ client }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
